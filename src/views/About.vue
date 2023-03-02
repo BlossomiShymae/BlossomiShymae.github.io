52b141951eb91caa100dc1e150758767edc593c4
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-01 21:05:19 -0600

    Update about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,37 @@
-<script setup lang="ts">
-  
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div id="about">
     <h1>About Blossomi Shymae</h1>
-    <span class="badge bg-secondary rounded-pill mb-2 me-2 text-pink fw-bolder">she/her</span><span class="badge bg-secondary rounded-pill mb-2 text-greenish fw-bolder">INFP</span>
+    <span class="badge bg-secondary rounded-pill mb-2 me-2 text-pink fw-bolder"
+      >she/her</span
+    ><span class="badge bg-secondary rounded-pill mb-2 text-greenish fw-bolder"
+      >INFP-T</span
+    >
 
-    <p>Shymae is the first name of the pseudonym, hehe. I'm a very shy trans lady that wants to be herself. I primarily code but I also like to digitally draw sometimes.... {{ `>.<` }}</p>
-    <p>I'm literally a real life <a href="https://anilist.co/character/257562/Hitori-Gotou">Hitori Goto</a> or <a href="https://anilist.co/character/128986/Bocchi-Hitori">Bocchi Hitori</a> but older.
-    It is very hard to share, talk, or discuss anything without catastrophizing. The feeling of wanting friends but also being unable to be yourself is very heartbreaking.... :c</p>
-    
-    <p>I can be found in the <a href="https://discord.com/invite/riotgamesdevrel">Riot Games Third Party Developer Community Discord</a>.</p>
+    <p>
+      Shymae is the first name of the pseudonym, hehe. I'm a very shy trans lady
+      that wants to be herself. I primarily code but I also like to digitally
+      draw sometimes.... {{ `>.<` }}
+    </p>
+    <p>
+      I'm literally a real life
+      <a href="https://anilist.co/character/257562/Hitori-Gotou">Hitori Goto</a>
+      or
+      <a href="https://anilist.co/character/128986/Bocchi-Hitori"
+        >Bocchi Hitori</a
+      >
+      but older. It is very hard to share, talk, or discuss anything without
+      catastrophizing. The feeling of wanting friends but also being unable to
+      be yourself is very heartbreaking.... :c
+    </p>
+
+    <p>
+      I can be found in the
+      <a href="https://discord.com/invite/riotgamesdevrel"
+        >Riot Games Third Party Developer Community Discord</a
+      >.
+    </p>
     <p class="fw-bold">BlossomiShymae<span class="text-muted">#7109</span></p>
   </div>
 </template>
