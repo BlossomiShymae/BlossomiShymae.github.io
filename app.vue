773dafc5540b806bc17cee56c84d7505932f3115
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-07-07 15:52:31 -0500

    ✏ Add spacing

--- app.vue
+++ app.vue
@@ -155,7 +155,7 @@
     <div class="container">
       <footer class="py-3 my-4">
         <p class="text-center text-body-secondary">
-          © 2023 BlossomiShymae 🌸💔
+          © 2023 Blossomi Shymae 🌸💔
         </p>
       </footer>
     </div>
