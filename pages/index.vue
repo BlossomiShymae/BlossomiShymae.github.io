f6674afdca1cdd923515d82050bd5bc3ed392dd8
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-07-07 05:43:19 -0500

    ⭐ Update home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="px-4 py-5 my-5 text-center">
-    <div class="img-fluid">
-      <img
-        class="d-block mx-auto mb-4"
-        src="/img/side_hero.png"
-        style="width: 300px"
-      />
-    </div>
+  <div class="px-4 pt-5 my-5 text-center border-bottom">
     <h1 class="display-5 fw-bold">blossomishymae.github.io</h1>
     <div class="col-lg-6 mx-auto">
       <p class="lead mb-4">
         Totes radical homepage of your shy gloomy elf girl.
+        <span class="fw-normal"
+          ><small>Now with a new makeover! 💜</small></span
+        >
       </p>
-      <p class="fs-tiny">Derp sheep not included.</p>
+    </div>
+    <div class="overflow-hidden" style="max-height: 33vh">
+      <div class="container px-5">
+        <img class="img-fluid mb-4" src="/img/pinup.png" loading="lazy" />
+      </div>
+    </div>
+  </div>
+  <div class="container col-xxl-8 px-4 py-5">
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div class="col-10 col-sm-8 col-lg-6">
+        <img
+          class="d-block mx-lg-auto img-fluid"
+          src="/img/side_hero.png"
+          loading="lazy"
+        />
+      </div>
+      <div class="col-lg-6">
+        <p class="lead mb-0">Previous design.</p>
+        <small class="fs-tiny">Derp sheep not included.</small>
+      </div>
     </div>
   </div>
 </template>
