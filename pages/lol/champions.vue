44ebb82fe8b2d6438fca262853d14bb98ae63ed8
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-07-08 01:20:56 -0500

    💻 Use client only for League pages

--- pages/lol/champions.vue
+++ pages/lol/champions.vue
@@ -1,35 +1,37 @@
 <template>
-  <div class="px-4 py-4">
-    <h2>League of Legends Champions Viewer</h2>
-    <h5 class="text-muted">Patch {{ version }}</h5>
-    <table class="table table-striped table-hover">
-      <thead>
-        <th scope="col">ID</th>
-        <th scope="col">Icon</th>
-        <th scope="col">Key</th>
-        <th scope="col">Name</th>
-        <th scope="col">Title</th>
-      </thead>
-      <tbody>
-        <tr v-for="champion in champions" :id="champion.key">
-          <th scope="row">{{ champion.key }}</th>
-          <td>
-            <div class="img-fluid">
-              <img
-                :src="`http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champion.image.full}`"
-                style="width: 24px"
-                class="rounded"
-                loading="lazy"
-              />
-            </div>
-          </td>
-          <td>{{ champion.id }}</td>
-          <td>{{ champion.name }}</td>
-          <td>{{ champion.title }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <ClientOnly>
+    <div class="px-4 py-4">
+      <h2>League of Legends Champions Viewer</h2>
+      <h5 class="text-muted">Patch {{ version }}</h5>
+      <table class="table table-striped table-hover">
+        <thead>
+          <th scope="col">ID</th>
+          <th scope="col">Icon</th>
+          <th scope="col">Key</th>
+          <th scope="col">Name</th>
+          <th scope="col">Title</th>
+        </thead>
+        <tbody>
+          <tr v-for="champion in champions" :id="champion.key">
+            <th scope="row">{{ champion.key }}</th>
+            <td>
+              <div class="img-fluid">
+                <img
+                  :src="`http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champion.image.full}`"
+                  style="width: 24px"
+                  class="rounded"
+                  loading="lazy"
+                />
+              </div>
+            </td>
+            <td>{{ champion.id }}</td>
+            <td>{{ champion.name }}</td>
+            <td>{{ champion.title }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </ClientOnly>
 </template>
 
 <script lang="ts" setup>
