87ca281c047b20e217f509d1fd537796bc2bc06f
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-07-11 05:36:45 -0500

    🕳 Wah

---
+++ docs/_nuxt/pageless.22666bd1.js
@@ -0,0 +1,1 @@
+import{_ as s,o,c as t,r}from"./entry.edf27017.js";const a={},c={id:"pageless-layout"};function n(e,l){return o(),t("div",c,[r(e.$slots,"default")])}const d=s(a,[["render",n]]);export{d as default};
--- docs/_nuxt/pageless.718da79f.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o,c as t,r}from"./entry.ad24383c.js";const a={},c={id:"pageless-layout"};function n(e,l){return o(),t("div",c,[r(e.$slots,"default")])}const d=s(a,[["render",n]]);export{d as default};
