8ca215fa51b7a8f1b440fab66a794a516eee953c
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-07-12 07:58:32 -0500

    🖼 Make transitions pwettier

--- app.vue
+++ app.vue
@@ -7,21 +7,22 @@
 <style>
 .page-enter-active,
 .page-leave-active {
-  transition: all 0.125s;
+  transition: all 0.2s;
 }
 .page-enter-from,
 .page-leave-to {
   opacity: 0;
-  filter: blur(1rem);
+  filter: blur(0.125rem);
 }
 
 .layout-enter-active,
 .layout-leave-active {
-  transition: all 0.125s;
+  transform: none;
+  transition: all 0.5s;
 }
 .layout-enter-from,
 .layout-leave-to {
-  filter: grayscale(1);
+  transform: translateX(-100%);
 }
 </style>
 
