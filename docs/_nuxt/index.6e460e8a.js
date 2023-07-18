c408434b7985962dc2b65600baef953d4ae45291
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-07-18 15:57:04 -0500

    💔

---
+++ docs/_nuxt/index.6e460e8a.js
@@ -0,0 +1,1 @@
+import{r as u,z as c,u as f,A as l,B as p}from"./entry.6ae515d7.js";function d(t,a){const e=u({});return c(()=>{const s=l(t),{title:n,titleTemplate:o,...r}=s;e.value={title:n,titleTemplate:o,meta:p(r)}}),f(e,a)}export{d as u};
