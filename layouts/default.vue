f62401b4de7fb1625f7df13be546146b07cff01f
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-09-26 15:29:57 -0500

    Update layout (totes broken on mobile)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,108 @@
 <template>
-  <div>
+  <div class="position-relative">
+    <aside class="d-flex flex-column justify-content-start align-items-center p-4 border border-accent border-2 gap-2">
+      <div class="d-flex justify-content-center gap-2">
+        <NuxtLink to="/" class="btn bg-accent-20 text-decoration-none hover-accent-dim">Home</NuxtLink>
+        <NuxtLink to="/blog/programming-thingies" class="btn bg-accent-20 text-decoration-none hover-accent-dim">Progamming Thingies</NuxtLink>
+      </div>
+      <div class="d-flex justify-content-center gap-2">
+        <NuxtLink to="/lol/champions" class="btn bg-accent-20 text-decoration-none hover-accent-dim">Champions</NuxtLink>
+        <NuxtLink to="/lol/items" class="btn bg-accent-20 text-decoration-none hover-accent-dim">Items</NuxtLink>
+        <NuxtLink to="/lol/runes" class="btn bg-accent-20 text-decoration-none hover-accent-dim">Runes</NuxtLink>
+        <NuxtLink to="/lol/summoner-icons" class="btn bg-accent-20 text-decoration-none hover-accent-dim">Summoner Icons</NuxtLink>
+      </div>
+      <!-- <ul
+        class="mb-2 pb-3 me-2 list-unstyled flex-column border-bottom border-accent"
+      >
+        <li class="mb-2">
+          <NuxtLink
+            to="/"
+            class="btn bg-accent-20 fw-semibold rounded-pill text-decoration-none hover-accent-dim w-100 text-start"
+          >
+            <PhHouse :size="24" color="black" weight="fill" />
+            Home
+          </NuxtLink>
+        </li>
+        <li class="mb-2">
+          <NuxtLink
+            to="/blog/programming-thingies"
+            class="btn bg-accent-20 fw-semibold rounded-pill text-decoration-none hover-accent-dim w-100 text-start"
+          >
+            <PhKeyboard :size="24" color="black" weight="fill" />
+            Programming Thingies
+          </NuxtLink>
+        </li>
+        <li clas="mb-2">
+          <button
+            class="btn btn-toggle bg-accent-secondary-20 fw-semibold rounded-pill text-decoration-none hover-accent-secondary-dim w-100 text-start"
+            data-bs-toggle="collapse"
+            data-bs-target="#lol-collapse"
+          >
+            <PhArrowRight :size="24" color="black" weight="fill" />
+            League of Legends Data
+          </button>
+          <div id="lol-collapse" class="collapse">
+            <NuxtLink
+              to="/lol/items"
+              class="mt-2 mb-2 btn bg-accent-20 fw-semibold rounded-pill text-decoration-none hover-accent-dim w-100 text-start"
+            >
+              <PhBoot :size="24" color="black" weight="fill" />
+              Items
+            </NuxtLink>
+            <NuxtLink
+              to="/lol/champions"
+              class="mb-2 btn bg-accent-20 fw-semibold rounded-pill text-decoration-none hover-accent-dim w-100 text-start"
+            >
+              <PhUser :size="24" color="black" weight="fill" />
+              Champions
+            </NuxtLink>
+            <NuxtLink
+              to="/lol/runes"
+              class="mb-2 btn bg-accent-20 fw-semibold rounded-pill text-decoration-none hover-accent-dim w-100 text-start"
+            >
+              <PhCirclesThree :size="24" color="black" weight="fill" />
+              Runes
+            </NuxtLink>
+            <NuxtLink
+              to="/lol/summoner-icons"
+              class="mb-2 btn bg-accent-20 fw-semibold rounded-pill text-decoration-none hover-accent-dim w-100 text-start"
+            >
+              <PhUserCircle :size="24" color="black" weight="fill" />
+              Summoner Icons
+            </NuxtLink>
+          </div>
+        </li>
+      </ul>
+      <div class="d-flex justify-content-center w-100">
+        
+      </div> -->
+      <div class="border-top border-accent border-2 mt-3 pt-2">
+        <p>
+          Now with
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 135 90"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class="relative w-5 h-5 u-text-gray-200"
+          >
+            <title>Nuxt logo</title>
+            <path
+              d="M74.2284 87.8731H123.414C124.977 87.8733 126.511 87.4745 127.864 86.7167C129.217 85.959 130.341 84.869 131.121 83.5565C131.902 82.244 132.313 80.7552 132.312 79.2398C132.312 77.7245 131.899 76.2361 131.118 74.9242L98.0856 19.4276C97.3048 18.1154 96.1817 17.0257 94.829 16.2681C93.4764 15.5105 91.942 15.1116 90.38 15.1116C88.8181 15.1116 87.2836 15.5105 85.931 16.2681C84.5784 17.0257 83.4552 18.1154 82.6745 19.4276L74.2284 33.6272L57.7148 5.86031C56.9334 4.54821 55.8097 3.45868 54.4567 2.70119C53.1037 1.9437 51.569 1.54492 50.0068 1.54492C48.4446 1.54492 46.9099 1.9437 45.5569 2.70119C44.2039 3.45868 43.0803 4.54821 42.2989 5.86031L1.1947 74.9242C0.41273 76.2361 0.000680577 77.7245 8.42355e-07 79.2398C-0.000678893 80.7552 0.410035 82.244 1.19083 83.5565C1.97163 84.869 3.09498 85.959 4.44789 86.7167C5.80081 87.4745 7.33558 87.8733 8.89785 87.8731H39.7727C52.0057 87.8731 61.0271 82.661 67.2346 72.4925L82.3054 47.1759L90.3776 33.6272L114.604 74.3244H82.3054L74.2284 87.8731ZM39.2694 74.3105L17.7227 74.3058L50.0212 20.046L66.1369 47.1759L55.3468 65.309C51.2244 71.9067 46.5411 74.3105 39.2694 74.3105Z"
+              fill="#00DC82"
+            ></path>
+          </svg>
+          <span class="fw-bold">Nuxt</span>!
+        </p>
+      </div>
+    </aside>
+
     <header
-      class="bg-accent bg-accent-20 sticky-top py-2 border-bottom"
-      style="z-index: 2000; backdrop-filter: blur(4px)"
+      class="sticky-top py-2 border-bottom border-accent"
     >
-      <div class="container container-grid">
-        <div class="grid-aside w-100">
-          <div class="d-flex align-content-center align-items-center w-100">
+    <nav class="d-flex justify-content-around align-items-center flex-wrap">
+          <div class="d-flex">
             <div class="me-2 rounded-circle bg-accent-secondary-20">
               <img src="/favicon.png" class="rounded-circle p-1" id="favicon" />
             </div>
@@ -14,8 +110,8 @@
               blossomishymae.github.io
             </h3>
           </div>
-        </div>
-        <div class="grid-content ps-4 w-100">
+
+          <div class="d-flex justify-content-end flex-wrap">
           <div
             class="d-flex justify-content-end align-items-center w-100 h-100"
           >
@@ -66,105 +162,16 @@
             </a>
           </div>
         </div>
-      </div>
+    </nav>
     </header>
 
-    <main class="container container-grid">
-      <aside id="aside-parent">
-        <nav
-          id="aside-navbar"
-          class="sticky-top overflow-y-auto h-100 d-flex flex-column py-3"
-        >
-          <ul
-            class="mb-2 pb-3 me-2 list-unstyled flex-column border-bottom border-accent"
-          >
-            <li class="mb-2">
-              <NuxtLink
-                to="/"
-                class="btn bg-accent-20 fw-semibold rounded-pill text-decoration-none hover-accent-dim w-100 text-start"
-              >
-                <PhHouse :size="24" color="black" weight="fill" />
-                Home
-              </NuxtLink>
-            </li>
-            <li class="mb-2">
-              <NuxtLink
-                to="/blog/programming-thingies"
-                class="btn bg-accent-20 fw-semibold rounded-pill text-decoration-none hover-accent-dim w-100 text-start"
-              >
-                <PhKeyboard :size="24" color="black" weight="fill" />
-                Programming Thingies
-              </NuxtLink>
-            </li>
-            <li clas="mb-2">
-              <button
-                class="btn btn-toggle bg-accent-secondary-20 fw-semibold rounded-pill text-decoration-none hover-accent-secondary-dim w-100 text-start"
-                data-bs-toggle="collapse"
-                data-bs-target="#lol-collapse"
-              >
-                <PhArrowRight :size="24" color="black" weight="fill" />
-                League of Legends Data
-              </button>
-              <div id="lol-collapse" class="collapse">
-                <NuxtLink
-                  to="/lol/items"
-                  class="mt-2 mb-2 btn bg-accent-20 fw-semibold rounded-pill text-decoration-none hover-accent-dim w-100 text-start"
-                >
-                  <PhBoot :size="24" color="black" weight="fill" />
-                  Items
-                </NuxtLink>
-                <NuxtLink
-                  to="/lol/champions"
-                  class="mb-2 btn bg-accent-20 fw-semibold rounded-pill text-decoration-none hover-accent-dim w-100 text-start"
-                >
-                  <PhUser :size="24" color="black" weight="fill" />
-                  Champions
-                </NuxtLink>
-                <NuxtLink
-                  to="/lol/runes"
-                  class="mb-2 btn bg-accent-20 fw-semibold rounded-pill text-decoration-none hover-accent-dim w-100 text-start"
-                >
-                  <PhCirclesThree :size="24" color="black" weight="fill" />
-                  Runes
-                </NuxtLink>
-                <NuxtLink
-                  to="/lol/summoner-icons"
-                  class="mb-2 btn bg-accent-20 fw-semibold rounded-pill text-decoration-none hover-accent-dim w-100 text-start"
-                >
-                  <PhUserCircle :size="24" color="black" weight="fill" />
-                  Summoner Icons
-                </NuxtLink>
-              </div>
-            </li>
-          </ul>
-          <div class="d-flex justify-content-center w-100">
-            <p>
-              Now with
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 135 90"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                class="relative w-5 h-5 u-text-gray-200"
-              >
-                <title>Nuxt logo</title>
-                <path
-                  d="M74.2284 87.8731H123.414C124.977 87.8733 126.511 87.4745 127.864 86.7167C129.217 85.959 130.341 84.869 131.121 83.5565C131.902 82.244 132.313 80.7552 132.312 79.2398C132.312 77.7245 131.899 76.2361 131.118 74.9242L98.0856 19.4276C97.3048 18.1154 96.1817 17.0257 94.829 16.2681C93.4764 15.5105 91.942 15.1116 90.38 15.1116C88.8181 15.1116 87.2836 15.5105 85.931 16.2681C84.5784 17.0257 83.4552 18.1154 82.6745 19.4276L74.2284 33.6272L57.7148 5.86031C56.9334 4.54821 55.8097 3.45868 54.4567 2.70119C53.1037 1.9437 51.569 1.54492 50.0068 1.54492C48.4446 1.54492 46.9099 1.9437 45.5569 2.70119C44.2039 3.45868 43.0803 4.54821 42.2989 5.86031L1.1947 74.9242C0.41273 76.2361 0.000680577 77.7245 8.42355e-07 79.2398C-0.000678893 80.7552 0.410035 82.244 1.19083 83.5565C1.97163 84.869 3.09498 85.959 4.44789 86.7167C5.80081 87.4745 7.33558 87.8733 8.89785 87.8731H39.7727C52.0057 87.8731 61.0271 82.661 67.2346 72.4925L82.3054 47.1759L90.3776 33.6272L114.604 74.3244H82.3054L74.2284 87.8731ZM39.2694 74.3105L17.7227 74.3058L50.0212 20.046L66.1369 47.1759L55.3468 65.309C51.2244 71.9067 46.5411 74.3105 39.2694 74.3105Z"
-                  fill="#00DC82"
-                ></path>
-              </svg>
-              <span class="fw-bold">Nuxt</span>!
-            </p>
-          </div>
-        </nav>
-      </aside>
-      <article id="main-content" class="order-1 grid-content ps-4 pb-4">
-        <slot></slot>
-      </article>
+  
+
+    <main class="container">
+      <slot></slot>
     </main>
-    <div class="container">
-      <footer class="py-3 my-4">
+
+    <footer class="py-3 my-4">
         <p class="text-center text-body-secondary">
           © 2023 Blossomi Shymae 🌸💔
         </p>
@@ -174,7 +181,6 @@
           endorse or sponsor this project.
         </p>
       </footer>
-    </div>
   </div>
 </template>
 
