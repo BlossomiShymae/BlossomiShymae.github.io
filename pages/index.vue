1485821ff7d24ab53df4a77292a367cfcde2fc98
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2024-05-05 10:09:53 -0500

    Update home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,50 @@
 <template>
-  <div class="row">
-    <div class="col-lg-6">
-      <h2 class="fw-light display-4">Blossomi Shymae</h2>
-      <h4 class="mb-4 pb-2 border-bottom"><span class="text-accent-secondary fw-bold">ブロソミー・シャイマエ</span></h4>
+  <div>
+    <div class="row">
+      <div class="col-lg-6">
+        <div style="width: 320px;">
+          <img class="img-fluid" src="/img/blossomi_logo.png" />
+        </div>
+        <h4 class="mb-4 pb-2 border-bottom"><span class="text-accent-secondary fw-bold">ブロソミー・シャイマエ</span></h4>
 
-      <p class="lead">Hiii! I'm Blossomi Shymae, your shy lonely elf girl.
-         I chat with friends and do totes random things, such as gaming, drawing, programming, and surfing the web.</p>
+        <p class="lead">Hiii! I'm Blossomi Shymae, your shy lonely elf girl.
+          I chat with friends and do totes random things, such as gaming, drawing, programming, and surfing the web.</p>
 
+      </div>
+      <div class="col-lg-6">
+        <div class="d-flex justify-content-center align-items-center mb-4">
+          <div style="width: 450px;">
+            <img src="/img/banner_2.png" class="img-fluid"/>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="col-lg-6">
-      <div class="d-flex justify-content-center align-items-center mb-4">
-        <div style="width: 450px;">
-          <img src="/img/banner_2.png" class="img-fluid"/>
+
+    <div class="divider"></div>
+
+    <div class="row">
+      <div class="col-lg-6 d-flex justify-content-center">
+        <div style="width: 320px;">
+          <img class="img-fluid" src="/img/blossomi_peek.png" />
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <h1 class="border-bottom pb-2">Background</h1>
+        <p class="lead">I'm a trans girl that is continuing the journey in discovering herself. 
+          I hope to be a content creator one day but I'm not comfortable in doing so yet. </p>
+      </div>
+    </div>
+
+    <div class="divider"></div>
+
+    <div class="row">
+      <div class="col-lg-6">
+        <h1 class="border-bottom pb-2">Contact</h1>
+        <p class="lead"><a href="https://old.reddit.com/user/BlossomiShymae/" class="text-light">Send a private message (not chat) to my Reddit!</a></p>
+      </div>
+      <div class="col-lg-6 d-flex justify-content-center">
+        <div style="width: 320px">
+          <img class="img-fluid" src="/img/blossomi_uwu.png"/>
         </div>
       </div>
     </div>
