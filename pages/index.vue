e5a797318b4cc2ffc4d13057bc3d3cc6ed579f6f
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2024-05-11 01:17:13 -0500

    Add social section

--- pages/index.vue
+++ pages/index.vue
@@ -31,7 +31,8 @@
       <div class="col-lg-6">
         <h1 class="border-bottom pb-2">Background</h1>
         <p class="lead">I'm a trans girl that is continuing the journey in discovering herself. 
-          I hope to be a content creator one day but I'm not comfortable in doing so yet. </p>
+          I hope to be a content creator one day but I'm not comfortable in doing so yet.
+          As of now, I primarily make open-source programming tools and apps for the Riot Games Third Party Developer Community. </p>
       </div>
     </div>
 
@@ -39,15 +40,36 @@
 
     <div class="row">
       <div class="col-lg-6">
-        <h1 class="border-bottom pb-2">Contact</h1>
-        <p class="lead"><a href="https://old.reddit.com/user/BlossomiShymae/" class="text-light">Send a private message (not chat) to my Reddit!</a></p>
+        <h1 class="border-bottom pb-2">Socials</h1>
+        <div class="d-flex flex-column align-items-stretch gap-3">
+          <a href="https://reddit.com/u/BlossomiShymae" class="text-decoration-none text-light lead bg-right-gradient ps-2">/u/BlossomiShymae</a>
+          <a href="https://github.com/BlossomiShymae" class="text-decoration-none text-light lead bg-right-gradient ps-2">GitHub</a>
+          <a href="https://tumblr.com/blossomishymae" class="text-decoration-none text-light lead bg-right-gradient ps-2">Tumblr</a>
+          <a href="https://twitter.com/BlossomiShymae" class="text-decoration-none text-light lead bg-right-gradient ps-2">X / Twitter</a>
+        </div>
       </div>
+      <div class="col-lg-6 d-flex justify-content-center">
+        <div style="width: 450px;">
+          <img class="img-fluid" src="/img/may_2024.png"/>
+        </div>
+      </div>
+    </div>
+
+    <div class="divider"></div>
+
+    <div class="row">
       <div class="col-lg-6 d-flex justify-content-center">
         <div style="width: 320px">
           <img class="img-fluid" src="/img/blossomi_uwu.png"/>
         </div>
       </div>
+      <div class="col-lg-6">
+        <h1 class="border-bottom pb-2">Contact</h1>
+        <p class="lead"><a href="https://old.reddit.com/user/BlossomiShymae/" class="text-light">Send a private message (not chat) to my Reddit!</a></p>
+      </div>
     </div>
+
+    
   </div>
 </template>
 
