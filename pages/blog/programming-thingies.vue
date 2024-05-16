ecda3ab91ea69fa0793efa53f0a6607edee31a42
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2024-05-16 16:37:42 -0500

    Fix background images

--- pages/blog/programming-thingies.vue
+++ pages/blog/programming-thingies.vue
@@ -10,35 +10,35 @@
     </p>
     <div class="d-flex flex-wrap" style="gap: 18px">
       <LinkCard
-        src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/498/498004.jpg"
+        src="https://raw.communitydragon.org/14.9/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/498/498004.jpg"
         url="https://github.com/BlossomiShymae/clean-cuts"
         >
         <h3 class="fw-bold">Clean Cuts</h3>
         Making game data viewable in a human-friendly format. 
       </LinkCard>
       <LinkCard
-        src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/163/163003.jpg"
+        src="https://raw.communitydragon.org/14.9/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/163/163003.jpg"
         url="https://github.com/BlossomiShymae/barcodes"
         >
         <h3 class="fw-bold">barcodes</h3>
         Display your pride in the terminal
       </LinkCard>
       <LinkCard
-        src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/loadouts/companions/tooltip_chibigwen_base_classic_tier1.png"
+        src="https://raw.communitydragon.org/14.9/plugins/rcp-be-lol-game-data/global/default/assets/loadouts/companions/tooltip_chibigwen_base_classic_tier1.png"
         url="https://github.com/BlossomiShymae/Needlework"
       >
         <h3 class="fw-bold">Needlework</h3>
         A helper development tool for the LCU!
       </LinkCard>
       <LinkCard
-        src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/99/99006.jpg"
+        src="https://raw.communitydragon.org/14.9/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/99/99006.jpg"
         url="https://github.com/BlossomiShymae/llux"
       >
         <h3 class="fw-bold">llux</h3>
         Transfer data to the LCU via the command-line interface! 🌟
       </LinkCard>
       <LinkCard
-        src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/887/887000.jpg"
+        src="https://raw.communitydragon.org/14.9/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/887/887000.jpg"
         url="https://github.com/BlossomiShymae/RiotBlossom"
       >
         <h3 class="fw-bold">RiotBlossom</h3>
@@ -46,14 +46,14 @@
         API to make things totes easier!
       </LinkCard>
       <LinkCard
-        src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/887/887020.jpg"
+        src="https://raw.communitydragon.org/14.9/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/887/887020.jpg"
         url="https://github.com/BlossomiShymae/snip-snip"
       >
         <h3 class="fw-bold">snip-snip</h3>
         A CommunityDragon recursive directory downloader tool! ✂
       </LinkCard>
       <LinkCard
-        src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/104/104005.jpg"
+        src="https://raw.communitydragon.org/14.9/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/104/104005.jpg"
         url="https://github.com/DarkIntaqt/challenges"
       >
         <h3 class="fw-bold">
@@ -64,14 +64,14 @@
         thingies! Owned by DarkIntaqt.com.
       </LinkCard>
       <LinkCard
-        src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/loadouts/companions/tooltip_chibigwen_soulfighter_soulfighter_tier1.png"
+        src="https://raw.communitydragon.org/14.9/plugins/rcp-be-lol-game-data/global/default/assets/loadouts/companions/tooltip_chibigwen_soulfighter_soulfighter_tier1.png"
         url="https://github.com/BlossomiShymae/skip-n-slash"
       >
         <h3 class="fw-bold">skip-n-slash</h3>
         A desktop version of the raw.communitydragon.org search bar.
       </LinkCard>
       <LinkCard
-        src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/40/40046.jpg"
+        src="https://raw.communitydragon.org/14.9/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/40/40046.jpg"
         url="https://github.com/BlossomiShymae/monsoon"
       >
         <h3 class="fw-bold">monsoon</h3>
@@ -79,7 +79,7 @@
         balance changes.
       </LinkCard>
       <LinkCard
-        src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/145/145048.jpg"
+        src="https://raw.communitydragon.org/14.9/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/145/145048.jpg"
         url="https://github.com/TheDrone7/shieldbow"
       >
         <h3 class="fw-bold">
